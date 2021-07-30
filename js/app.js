afd30f2ad065c2e6b0004acead555b64d14219a3
calavera <fmontani81@gmail.com>
2021-07-30 16:14:56 +0200

    andato avanti

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 var app = new Vue({
     el: '#root',
     data: {
-        numbers: 0,
         preload: '',
-        lines: 10,
-        size: 70,
-        loadedPic: false 
-
+        size: 100,
+        loadedPic: false,
+        x: 0,
+        y: 0,
+        isPicSelected: false,
+        clickX: 0,
+        clickY: 0
     },
     methods: {
         preloadPic(event) {
             let f = event.target.files[0];
             this.preload = URL.createObjectURL(f);
             this.loadedPic = true;
+            const image = document.querySelector('img');
 
         },
-    },
+        selectPic(event) {
+            this.isPicSelected = true;
+            this.clickX = event.clientX;
+            this.clickY = event.clientY;
+        },
+        movePic(event) {
+            event.preventDefault();
+            if (this.isPicSelected) {
+                console.log(event.clientX);
+            }
+        },
+        move(event) {
+            if (this.isPicSelected) {
+                this.x = event.clientX - this.clickX;
+                this.y = event.clientY - this.clickY;
+            }
+        }
+     },
     mounted() {
     },
 })
